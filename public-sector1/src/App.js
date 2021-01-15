647996a816ae80154aa990f101609c27fe4019bb
Logan Arnett <logan@loganarnett.com>
2021-01-15 14:38:54 -0500

    updating sector 1

--- public-sector1/src/App.js
+++ public-sector1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Public Sector 2
+          Public Sector 1
         </h1>
         <a
           className="App-link"
